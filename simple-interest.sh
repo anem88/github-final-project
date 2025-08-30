1e2c1ef572e8479896d1922bf24a0fd696f6d05f
anem88 <anemkovich@gmail.com>
2025-08-30 22:53:36 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# anen88
+# anem88
 # Input:
 # p, principal amount
 # t, time period in years
